fda31463b4c204359fdd970b0390792711cf5e68
Luisa Cotte <luisacotte18@hotmail.com>
2020-10-13 23:12:13 -0500

    create main class

---
+++ src/co/uniquindio/edu/ipcalculator/Main.kt
@@ -0,0 +1,18 @@
+package co.uniquindio.edu.ipcalculator
+
+import javafx.application.Application
+import javafx.stage.Stage
+
+class Main: Application() {
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>){
+            launch(Main::class.java)
+        }
+    }
+
+    override fun start(primaryStage: Stage?) {
+        TODO("Not yet implemented")
+    }
+}
