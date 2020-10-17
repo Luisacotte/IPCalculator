b82652d1fb3e72d66feca07fe6fa11b199ad78c0
Crisi <harmaharcri@hotmail.com>
2020-10-17 17:12:41 -0500

    Added the methods to the assignable IP addresses and the complete range

--- src/co/uniquindio/edu/ipcalculator/Main.kt
+++ src/co/uniquindio/edu/ipcalculator/Main.kt
@@ -7,13 +7,16 @@ import javafx.stage.Stage
 class Main: Application() {
 
     override fun start(primaryStage: Stage?) {
-        var ipCalculator:IPCalculator = IPCalculator("173.34.64.0/19")
-        println(ipCalculator.getAddress())
-        println(ipCalculator.getSubnetMask())
+        var ipCalculator:IPCalculator = IPCalculator("184.35.77.15/22")
+        println(ipCalculator.getNetIPAddress())
         println(ipCalculator.getSubnetMaskInDecimalFormat())
         println(ipCalculator.getBroadcastAddress())
+        println(ipCalculator.getBitsNumberForTheNet())
+        println(ipCalculator.getBitsNumberForTheHost())
+        println(ipCalculator.getBitsNumberOfAssignableIPs())
+        println(ipCalculator.getRangeIPAddressComplete())
+        println(ipCalculator.getRangeIPAddressAssignable())
         System.exit(0)
-
     }
     companion object{
         @JvmStatic
--- src/co/uniquindio/edu/ipcalculator/model/IPCalculator.kt
+++ src/co/uniquindio/edu/ipcalculator/model/IPCalculator.kt
@@ -1,6 +1,7 @@
 package co.uniquindio.edu.ipcalculator.model
 
 import co.uniquindio.edu.ipcalculator.util.Util
+import kotlin.math.pow
 
 class IPCalculator(IPAddressComplete:String) {
 
@@ -14,7 +15,81 @@ class IPCalculator(IPAddressComplete:String) {
     }
 
     /**
-     * This method allows to
+     * This method allows to get the net IP address
+     * @return a String with the net IP address
+     */
+    fun getNetIPAddress():String{
+        var dotList:ArrayList<Int> = ArrayList()
+        dotList = Util.getDotsPositions(IPAddress, dotList, 0)
+        var firstOctect:String = Integer.toBinaryString(IPAddress.substring(0, dotList.get(0)).toInt())
+        var secondOctect:String = Integer.toBinaryString(IPAddress.substring(dotList.get(0)+1,dotList.get(1)).toInt())
+        var thirdOctect:String = Integer.toBinaryString(IPAddress.substring(dotList.get(1)+1,dotList.get(2)).toInt())
+        var fourthOctect:String = Integer.toBinaryString(IPAddress.substring(dotList.get(2)+1, IPAddress.length).toInt())
+        var ipAddress:String = firstOctect+secondOctect+thirdOctect+fourthOctect
+        var subnetMaskString:String = ""
+        for(i in 0..subnetMask){
+            subnetMaskString += "1"
+        }
+        var iLimit:Int = 32-subnetMask
+        for(i in 0..iLimit){
+            subnetMaskString += "0"
+        }
+        var firstOctect2:String = subnetMaskString.substring(0, 8)
+        var secondOctect2:String = subnetMaskString.substring(8,16)
+        var thirdOctect2:String = subnetMaskString.substring(16,24)
+        var fourthOctect2:String = subnetMaskString.substring(24, 32)
+        var subnetMask:String = firstOctect2+secondOctect2+thirdOctect2+fourthOctect2
+        var netIPAddress:String = ""
+        println(ipAddress.length)
+        println(subnetMask.length)
+        var copyAux:String = ""
+        for(i in 0..((8-firstOctect.length)-1)){
+            copyAux += "0"
+        }
+        copyAux += firstOctect
+        firstOctect = copyAux
+
+        var copyAux2:String = ""
+
+        for(i in 0..((8-secondOctect.length)-1)){
+            copyAux2 +="0"
+        }
+        copyAux2 += secondOctect
+        secondOctect = copyAux2
+
+        var copyAux3:String = ""
+        for(i in 0..((8-thirdOctect.length)-1)){
+            copyAux3 += "0"
+        }
+        copyAux3+= thirdOctect
+        thirdOctect = copyAux3
+
+        var copyAux4:String = ""
+        for(i in 0..((8-fourthOctect.length)-1)){
+            copyAux4 += 0
+        }
+        copyAux4 += fourthOctect
+        fourthOctect = copyAux4
+        ipAddress= firstOctect +secondOctect + thirdOctect+fourthOctect
+        for(i in 0..(subnetMask.length-1)){
+            if(subnetMask[i]=='1'){
+                netIPAddress += ipAddress[i]
+            }else{
+                netIPAddress += "0"
+            }
+        }
+        var firstOctect3:String = netIPAddress.substring(0, 8)
+        var secondOctect3:String = netIPAddress.substring(8,16)
+        var thirdOctect3:String = netIPAddress.substring(16,24)
+        var fourthOctect3:String = netIPAddress.substring(24, 32)
+        return  "".plus(firstOctect3.toLong(2))
+                .plus(".").plus(secondOctect3.toLong(2))
+                .plus(".").plus(thirdOctect3.toLong(2))
+                .plus(".").plus(fourthOctect3.toLong(2))
+    }
+
+    /**
+     * This method allows to get the subnetMask
      */
     fun getSubnetMaskInDecimalFormat(addressComplete: String):Int{
 
@@ -60,11 +135,12 @@ class IPCalculator(IPAddressComplete:String) {
      */
     fun getBroadcastAddress():String{
         var dotList:ArrayList<Int> = ArrayList()
-        dotList = Util.getDotsPositions(IPAddress, dotList, 0)
-        var firstOctect:String = Integer.toBinaryString(IPAddress.substring(0, dotList.get(0)).toInt())
-        var secondOctect:String = Integer.toBinaryString(IPAddress.substring(dotList.get(0)+1,dotList.get(1)).toInt())
-        var thirdOctect:String = Integer.toBinaryString(IPAddress.substring(dotList.get(1)+1,dotList.get(2)).toInt())
-        var fourthOctect:String = Integer.toBinaryString(IPAddress.substring(dotList.get(2)+1, IPAddress.length).toInt())
+        var ipAddress:String = getNetIPAddress()
+        dotList = Util.getDotsPositions(ipAddress, dotList, 0)
+        var firstOctect:String = Integer.toBinaryString(ipAddress.substring(0, dotList.get(0)).toInt())
+        var secondOctect:String = Integer.toBinaryString(ipAddress.substring(dotList.get(0)+1,dotList.get(1)).toInt())
+        var thirdOctect:String = Integer.toBinaryString(ipAddress.substring(dotList.get(1)+1,dotList.get(2)).toInt())
+        var fourthOctect:String = Integer.toBinaryString(ipAddress.substring(dotList.get(2)+1, ipAddress.length).toInt())
         var copyAux:String = ""
         for(i in 0..((8-firstOctect.length)-1)){
             copyAux += "0"
@@ -108,6 +184,59 @@ class IPCalculator(IPAddressComplete:String) {
                 .plus(".").plus(thirdOctect2.toLong(2))
                 .plus(".").plus(fourthOctect2.toLong(2))
 
+    }
+
+    /**
+     * This method allows to get the bits number of the net
+     * @return an int to the bits number for the net
+     */
+    fun getBitsNumberForTheNet():Int{
+        return subnetMask
+    }
+
+    /**
+     * This method allows to get the bits number of the host
+     * @return an int with the bits number of the host
+     */
+    fun getBitsNumberForTheHost():Int{
+        return 32-subnetMask
+    }
+
+    /**
+     * This method allows to get the bits number of the assignable IP addresses
+     * @return an int with the assignable IP addresses
+     */
+    fun getBitsNumberOfAssignableIPs():Int{
+        var bitsForTheHost:Int = getBitsNumberForTheHost()
+        var assignableIPs:Int = 0
+        if(bitsForTheHost>1){
+            assignableIPs = (2.0.pow(bitsForTheHost)-2).toInt()
+        }
+        return assignableIPs
+    }
+
+    /**
+     * This method allows to get the complete range of the IP addresses
+     * @return a String with the range of the IP addresses
+     */
+    fun getRangeIPAddressComplete():String{
+        return getNetIPAddress()+" - "+getBroadcastAddress()
+    }
+
+    /**
+     * This method allows to get the IP Assignable range
+     */
+    fun getRangeIPAddressAssignable():String{
+        var ipAddress:String = getNetIPAddress()
+        var broadcastAddress:String = getBroadcastAddress()
+        var dotList:ArrayList<Int> = ArrayList()
+        dotList = Util.getDotsPositions(ipAddress, dotList, 0)
+        var dotList2:ArrayList<Int> = ArrayList()
+        dotList2 = Util.getDotsPositions(broadcastAddress, dotList, 0)
+        var lowerLimit:Int = ipAddress.substring(dotList.get(dotList.size-1)+1, ipAddress.length).toInt() +1
+        var upperLimit:Int = broadcastAddress.substring(dotList2.get(dotList2.size-1)+1, broadcastAddress.length).toInt()-1
+        return ipAddress.substring(0, dotList.get(dotList.size-1))+"."+lowerLimit+
+                " - "+broadcastAddress.substring(0,dotList2.get(dotList2.size-4))+"."+upperLimit
     }
 
 }
