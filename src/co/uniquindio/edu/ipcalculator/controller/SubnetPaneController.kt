43ea21639fdbe524848576ca4047cb1ac008a92a
Luisa Cotte <luisacotte18@hotmail.com>
2020-10-28 22:50:59 -0500

    All points about subnet, with out validation

--- src/co/uniquindio/edu/ipcalculator/controller/SubnetPaneController.kt
+++ src/co/uniquindio/edu/ipcalculator/controller/SubnetPaneController.kt
@@ -1,4 +1,186 @@
 package co.uniquindio.edu.ipcalculator.controller
 
+import co.uniquindio.edu.ipcalculator.model.IPCalculator2
+import javafx.collections.FXCollections
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.Alert
+import javafx.scene.control.Label
+import javafx.scene.control.ListView
+import javafx.scene.control.TextField
+
 class SubnetPaneController {
+
+    private lateinit var ipCalculator: IPCalculator2
+
+    @FXML
+    private lateinit var ipAddressCompleteTxt: TextField
+
+    @FXML
+    private lateinit var bitQuantityFieldTxt: TextField
+
+    @FXML
+    private lateinit var principalNetAddressLabel: Label
+
+    @FXML
+    private lateinit var principalBroadcastAddressLabel: Label
+
+    @FXML
+    private lateinit var principalNumberHostLabel: Label
+
+    @FXML
+    private lateinit var principalNumberNetLabel: Label
+
+    @FXML
+    private lateinit var subnetList: ListView<String>
+
+    @FXML
+    private lateinit var subnetNumberTxt: TextField
+
+    @FXML
+    private lateinit var assignableSubnetRangeLabel: Label
+
+    @FXML
+    private lateinit var subnetRangeLabel: Label
+
+    @FXML
+    private lateinit var subnetAddressLabel: Label
+
+    @FXML
+    private lateinit var subnetBroadcastAddressLabel: Label
+
+    @FXML
+    private lateinit var hostNumberTxt: TextField
+
+    @FXML
+    private lateinit var hostAddressLabel: Label
+
+    @FXML
+    private lateinit var ipHostAddressTxt: TextField
+
+    @FXML
+    private lateinit var subnetAddressHostLabel: Label
+
+    @FXML
+    private lateinit var ipHost1Txt: TextField
+
+    @FXML
+    private lateinit var ipHost2Txt: TextField
+
+    @FXML
+    private lateinit var yesNotLabel: Label
+
+    @FXML
+    private lateinit var ipSubnetTxt: TextField
+
+    @FXML
+    private lateinit var addressQuantityTxt: TextField
+
+    @FXML
+    private lateinit var hostList: ListView<String>
+
+    @FXML
+    fun analyze2Host(event: ActionEvent) {
+        var auxBit = bitQuantityFieldTxt.text.toInt()
+        var aux = ipAddressCompleteTxt.text
+        ipCalculator = IPCalculator2(aux, auxBit)
+        var containsHost = ipCalculator.containsHost(ipHost1Txt.text,ipHost2Txt.text)
+        yesNotLabel.text = containsHost
+
+    }
+
+    @FXML
+    fun analyzeAddressHost(event: ActionEvent) {
+        var auxBit = bitQuantityFieldTxt.text.toInt()
+        var aux = ipAddressCompleteTxt.text
+        ipCalculator = IPCalculator2(aux, auxBit)
+        var getNumberHost = ipCalculator.subnetContainsHost(subnetNumberTxt.text,hostNumberTxt.text)
+        hostAddressLabel.text = getNumberHost
+    }
+
+    @FXML
+    fun analyzeAll(event: ActionEvent) {
+
+        var auxBit = bitQuantityFieldTxt.text.toInt()
+        var aux = ipAddressCompleteTxt.text
+        ipCalculator = IPCalculator2(aux, auxBit)
+
+        var broadcast = ipCalculator.getBroadcastAddress()
+        principalBroadcastAddressLabel.text = broadcast
+
+        var quantitySubnet = ipCalculator.getQuantitySubnet()
+        principalNumberNetLabel.text = quantitySubnet.toString()
+
+        var quantityHost = ipCalculator.getQuantityHost()
+        principalNumberHostLabel.text = quantityHost.toString()
+
+        var subnetsL = ipCalculator.getSubnetList()
+        subnetList.items = FXCollections.observableList(subnetsL)
+        subnetList.refresh()
+
+        principalNetAddressLabel.text = ipCalculator.getOnlySubnetList()[0]
+    }
+
+    @FXML
+    fun analyzeIpHostAddress(event: ActionEvent) {
+        var auxBit = bitQuantityFieldTxt.text.toInt()
+        var aux = ipAddressCompleteTxt.text
+        ipCalculator = IPCalculator2(aux, auxBit)
+        var ipAddressHost = ipCalculator.getSubnet(ipHostAddressTxt.text)
+        subnetAddressHostLabel.text = ipAddressHost
+    }
+
+    @FXML
+    fun analyzeQuantityHost(event: ActionEvent) {
+        var auxBit = bitQuantityFieldTxt.text.toInt()
+        var aux = ipAddressCompleteTxt.text
+        ipCalculator = IPCalculator2(aux, auxBit)
+        var quantity = addressQuantityTxt.text.toInt()
+        var hostL = ipCalculator.hostList(aux.split("/")[0])
+        hostList.items = FXCollections.observableList(hostL.slice(0 until quantity))
+        hostList.refresh()
+    }
+
+    @FXML
+    fun analyzeSubnet(event: ActionEvent) {
+
+        var auxBit = bitQuantityFieldTxt.text.toInt()
+        var aux = ipAddressCompleteTxt.text
+        ipCalculator = IPCalculator2(aux, auxBit)
+        var subnetNumber = subnetNumberTxt.text.toInt()
+        var subnetInfo = ipCalculator.getSubnetList()[subnetNumber].split("\t\t-\t\t")
+        var assignable = ipCalculator.assignableSubnetList(subnetNumber)
+
+        subnetAddressLabel.text = subnetInfo[0]
+        subnetBroadcastAddressLabel.text = subnetInfo[1]
+        subnetRangeLabel.text = subnetInfo[0] +" - "+subnetInfo[1]
+        assignableSubnetRangeLabel.text = assignable
+
+    }
+
+    /**
+     * Intento fallido de validación
+     */
+    fun isInputValid(): Boolean {
+        var valid = false
+        var errorMessage = ""
+        var alertType: Alert.AlertType = Alert.AlertType.NONE
+
+        if (subnetNumberTxt.text.isNullOrEmpty()) {
+            errorMessage += "Debes llenar todos los campos."
+            alertType = Alert.AlertType.WARNING
+        } else {
+            if (subnetNumberTxt.text.length <= ipCalculator.getSubnetList().size && subnetNumberTxt.text.length >= 0) {
+            } else {
+                errorMessage += "Sobrepasa la cantidad de Subredes\n"
+                alertType = Alert.AlertType.WARNING
+            }
+        }
+        if (errorMessage.isEmpty()) {
+            valid = true
+        } else {
+            RootViewController.showAlert(errorMessage, "INFORMACIÓN", "", alertType)
+        }
+        return valid
+    }
 }
---
+++ src/co/uniquindio/edu/ipcalculator/model/IPCalculator2.kt
@@ -0,0 +1,265 @@
+package co.uniquindio.edu.ipcalculator.model
+
+import co.uniquindio.edu.ipcalculator.exceptions.MalformedIPAddress
+import co.uniquindio.edu.ipcalculator.util.Util
+import kotlin.math.pow
+
+@Suppress("NAME_SHADOWING")
+class IPCalculator2(var IPAddress: String, var bit: Int) {
+
+    private var subnetMask: Int = 16
+
+    init {
+        setIPAddressAndSubnetMask(IPAddress)
+    }
+
+    /**
+     * This method allows to set the net address and the subnet mask only if they are well formed
+     * @throws MalformedIPAddress if: 1 the complete address is null or empty
+     *                                2 the complete address does not contains the / character
+     *                                3 some of the octects of the net address are major than 255
+     *                                4 the subnet mask is not on the range of [8, 255]
+     */
+    @Throws(MalformedIPAddress::class)
+    private fun setIPAddressAndSubnetMask(ipAddressComplete: String) {
+        if (!ipAddressComplete.isNullOrEmpty()) {
+            if (ipAddressComplete.contains("/")) {
+                var slashIndex = ipAddressComplete.indexOf('/')
+                IPAddress = ipAddressComplete.substring(0, slashIndex)
+                val array = IPAddress.split(".")
+                if (array[0].toInt() <= 255 && array[1].toInt() <= 255
+                        && array[2].toInt() <= 255
+                        && array[3].toInt() <= 255) {
+                    var auxSubnetMask = ipAddressComplete.substring(slashIndex + 1, ipAddressComplete.length).toInt()
+                    if (auxSubnetMask >= 8 && auxSubnetMask <= 32) {
+                        subnetMask = auxSubnetMask
+                    } else {
+                        throw MalformedIPAddress("La máscara de subred debe estar en el rango de 8 a 32")
+                    }
+                } else {
+                    throw MalformedIPAddress("La dirección IP está mal formada")
+                }
+            }
+        } else {
+            throw MalformedIPAddress("Debes ingresar la IP")
+        }
+    }
+
+    /**
+     * This method allows to get the broadcast address on the net
+     * @return the String with the correct format
+     */
+    fun getBroadcastAddress(): String {
+        var dotList: ArrayList<Int> = ArrayList()
+        var ipAddress: String = IPAddress.split("/")[0]
+        dotList = Util.getDotsPositions(ipAddress, dotList, 0)
+        var firstOctect: String = Integer.toBinaryString(ipAddress.substring(0, dotList.get(0)).toInt())
+        var secondOctect: String = Integer.toBinaryString(ipAddress.substring(dotList.get(0) + 1, dotList.get(1)).toInt())
+        var thirdOctect: String = Integer.toBinaryString(ipAddress.substring(dotList.get(1) + 1, dotList.get(2)).toInt())
+        var fourthOctect: String = Integer.toBinaryString(ipAddress.substring(dotList.get(2) + 1, ipAddress.length).toInt())
+        var copyAux: String = ""
+        for (i in 0..((8 - firstOctect.length) - 1)) {
+            copyAux += "0"
+        }
+        copyAux += firstOctect
+        firstOctect = copyAux
+
+        var copyAux2: String = ""
+
+        for (i in 0..((8 - secondOctect.length) - 1)) {
+            copyAux2 += "0"
+        }
+        copyAux2 += secondOctect
+        secondOctect = copyAux2
+
+        var copyAux3: String = ""
+        for (i in 0..((8 - thirdOctect.length) - 1)) {
+            copyAux3 += "0"
+        }
+        copyAux3 += thirdOctect
+        thirdOctect = copyAux3
+
+        var copyAux4: String = ""
+        for (i in 0..((8 - fourthOctect.length) - 1)) {
+            copyAux4 += 0
+        }
+        copyAux4 += fourthOctect
+        fourthOctect = copyAux4
+        var IPComplete: String = firstOctect + secondOctect + thirdOctect + fourthOctect
+
+        var broadCastAddress: String = IPComplete.substring(0, subnetMask)
+        for (i in subnetMask..32) {
+            broadCastAddress += "1"
+        }
+        var firstOctect2: String = broadCastAddress.substring(0, 8)
+        var secondOctect2: String = broadCastAddress.substring(8, 16)
+        var thirdOctect2: String = broadCastAddress.substring(16, 24)
+        var fourthOctect2: String = broadCastAddress.substring(24, 32)
+        return "".plus(firstOctect2.toLong(2))
+                .plus(".").plus(secondOctect2.toLong(2))
+                .plus(".").plus(thirdOctect2.toLong(2))
+                .plus(".").plus(fourthOctect2.toLong(2))
+
+    }
+
+    /**
+     * This method allows to get the subnets quantity
+     */
+    fun getQuantitySubnet(): Int {
+        return 2.toDouble().pow(bit).toInt()
+    }
+
+    /**
+     * This method allows to get the host quantity
+     */
+    fun getQuantityHost(): Int {
+        var mask = subnetMask
+        var bitHost = 32 - (mask + bit)
+        return 2.toDouble().pow(bitHost).toInt()
+    }
+
+    fun getSubnetList(): ArrayList<String> {
+        var array = ArrayList<String>()
+        array.add("Dirección de Subred\t\t-\t\tDirección de Broadcast")
+        var address = convertToBinary(IPAddress)
+        var a = address.substring(0,subnetMask)
+
+        for (i in 0 until getQuantitySubnet()){
+            var bina = Integer.toBinaryString(i)
+            var complete = completeZeros(bina,bit)
+            complete = a + complete
+
+            while (complete.length < 32){
+                complete += "0"
+            }
+            var netIP = binaryToAddressIP(complete)
+            var result = netIP + "\t\t-\t\t" + getBroadcastSubnet(netIP)
+            array.add(result)
+        }
+        return array
+    }
+
+    fun getOnlySubnetList(): ArrayList<String> {
+        var array = ArrayList<String>()
+        var address = convertToBinary(IPAddress)
+        var a = address.substring(0,subnetMask)
+
+        for (i in 0 until getQuantitySubnet()){
+            var bina = Integer.toBinaryString(i)
+            var complete = completeZeros(bina,bit)
+            complete = a + complete
+
+            while (complete.length < 32){
+                complete += "0"
+            }
+            var netIP = binaryToAddressIP(complete)
+            var result = netIP
+            array.add(result)
+        }
+        return array
+    }
+
+    private fun completeZeros(binary:String, digit:Int):String{
+        var newBinary = binary
+        while (newBinary.length < digit){
+            newBinary = "0$newBinary"
+        }
+        return newBinary
+    }
+
+    /**
+     * This method allows to convert the op address to binary
+     */
+    private fun convertToBinary(address: String): String {
+        var ipAddressBinary = ""
+        val partition = address.split(".")
+        for (i in partition) {
+            val aux = i.toInt()
+            var bin = Integer.toBinaryString(aux)
+            while (bin.length < 8) {
+                bin = "0$bin"
+            }
+            ipAddressBinary += bin
+        }
+        return ipAddressBinary
+    }
+
+    /**
+     * This method allows to get the subnet's broadcast
+     */
+    private fun getBroadcastSubnet(subnet:String): String {
+        var broadcastBinary = ""
+        val ipAddressBinary = convertToBinary(subnet)
+        for (i in ipAddressBinary.indices) broadcastBinary += if (i < bit+subnetMask) {
+            ipAddressBinary[i]
+        } else {
+            '1'
+        }
+        return binaryToAddressIP(broadcastBinary)
+    }
+
+    /**
+     * This method allows to convert the binary address ip to normal address ip
+     */
+    private fun binaryToAddressIP(bbinary: String): String {
+        var maskDecimal = ""
+        for (i in 0..3) {
+            val binary = bbinary.substring((i * 8) + 0, (i * 8) + 8)
+            maskDecimal += binary.toInt(2) // SistemasNumericos.binToDec(bin.toInt(2)) ??
+            if (i < 3) {
+                maskDecimal += '.'
+            }
+        }
+        return maskDecimal
+    }
+
+    fun assignableSubnetList(subnetNumber: Int): String {
+        var array = getSubnetList()[subnetNumber].split("\t\t-\t\t")
+        var first = array[0].split(".")
+        var second = array[1].split(".")
+        var net = first[3].toInt()+1
+        var broad = second[3].toInt()-1
+        var assignable = first[0] + "." + first[1] + "." + first[2] + "." + net.toString() + " - " +
+                second[0] + "." + second[1] + "." + second[2] + "." + broad.toString()
+        return assignable
+    }
+
+    fun hostList(subnet:String): List<String> {
+        var array = ArrayList<String>()
+        var bin = convertToBinary(subnet)
+        var a =  bin.substring(0,subnetMask+bit)
+        for (i in 0 until getQuantityHost()){
+            var converte = Integer.toBinaryString(i)
+            var complete = completeZeros(converte,32-(bit+subnetMask))
+            array.add(binaryToAddressIP(a+complete))
+        }
+        return array
+    }
+
+    fun subnetContainsHost(subnet:String,host:String):String{
+        var net = getOnlySubnetList()[subnet.toInt()-1]
+        var host1 = hostList(net)[host.toInt()]
+        return host1
+    }
+
+    fun containsHost(host1:String, host2:String):String{
+
+        if(getSubnet(host1) == getSubnet(host2)){
+            return "SI"
+        }
+        return "NO"
+    }
+
+    fun getSubnet(ip:String):String{
+        var broadcastBinary = ""
+        val ipAddressBinary = convertToBinary(ip)
+        for (i in ipAddressBinary.indices) broadcastBinary += if (i < bit+subnetMask) {
+            ipAddressBinary[i]
+        } else {
+            '0'
+        }
+        print(binaryToAddressIP(broadcastBinary))
+        return binaryToAddressIP(broadcastBinary)
+    }
+
+}
